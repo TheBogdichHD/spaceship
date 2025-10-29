83e911da466839edcd7320282e157b5c976f8372
TheBogdichHD <89939588+TheBogdichHD@users.noreply.github.com>
2025-10-29 20:44:21 +0300

    feat: make mock api server work regardless

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
 
-if (import.meta.env.DEV) {
-  const { makeServer } = await import("./services/mockServer");
-  makeServer({ environment: "development" });
-}
+// if (import.meta.env.DEV) {
+//   const { makeServer } = await import("./services/mockServer");
+//   makeServer({ environment: "development" });
+// }
+
+const { makeServer } = await import("./services/mockServer");
+makeServer({ environment: "development" });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
